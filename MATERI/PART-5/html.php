6300ff4c92ac2e9d3b8e9cda91625c90a7236802
Imam putra <imamap96@outlook.com>
2021-06-18 08:22:00 +0700

    update materi pertemuan 5

---
+++ MATERI/PART-5/html.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <title> belajar HTML pertemuan 5 </title>
+</head>
+
+<body>
+    <?php
+    echo "Hello ImamAP";
+
+    ?>
+</body>
+</html>
---
+++ MATERI/PART-5/index.php
@@ -0,0 +1,5 @@
+<?php
+//    echo "";
+   echo "<br>";
+   echo "Hello ImamAP";
+?>
