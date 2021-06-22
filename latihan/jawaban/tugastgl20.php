bcbf1a8b3620f6643e8a898aa05811ea1a76e338
imam961 <86098457+imam961@users.noreply.github.com>
2021-06-22 10:59:07 +0700

    Add files via upload

---
+++ latihan/jawaban/tugastgl20.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+    <h1> I. List item 1</h1>
+    <ol type = "a">
+    <li> Nested item 1.1 </li>
+    <li> Nested item 1.2 </li>
+    </ol>
+
+    <h1> II. List item 2</h1>
+    <ol>
+    <li> Nested item 2.1 </li>
+    <li> Nested item 2.2 </li>
+    </ol>
+    
+    <ul type = 'circle'>
+    <li> Nested item 2.2.1 </li>
+    <li> Nested item 2.2.2 </li>
+    <li> Nested item 2.2.3 </li>
+    </ul>
+    <ol>
+    <li> Nested item 2.3 </li>
+    </ol>
+
+    <h1> III. List item 3</h1>
+    <ul>
+    <li> Nested item 3.1 </li>
+    <li> Nested item 3.2 </li>
+    <li> Nested item 3.3 </li>
+    </ul>
+    </ol>
+</body>
+</html>
+
+
+
+
+        
+          
+
+
---
+++ latihan/jawaban/tugastgl21.php
@@ -0,0 +1,27 @@
+<form 
+
+<label>input text</label>     <input type="text" ><br><br>
+<label>input password</label> <input type="password" ><br><br>
+<label>input email</label>    <input type="email" ><br><br>
+<label>input number</label>   <input type="number" ><br><br>
+<label>input telp</label>     <input type="number" ><br><br>
+<label>input search</label>   <input type="search" ><br><br>
+<label>input url</label>      <input type="url" ><br><br>
+<label>input date</label>     <input type="date" ><br><br>
+<label>input time</label>     <input type="time" ><br><br>
+<label>input month</label>    <input type="month" ><br><br>
+<label>input datetime-lokal</label> <input type="datetime-local" ><br><br>
+<label>input week</label>     <input type="week" ><br><br>
+<label>input color</label>    <input type="color" ><br><br>
+<label>input range</label>    <input type="range" ><br><br>
+<label>input checkbox</label> <input type="checkbox" name="checkbox[]" value="1" /> 1 <input type="checkbox" name="checkbox[]" value="2" /> 2 <input type="checkbox" name="checkbox[]" value="3" /> 3 <br><br>
+<!-- <label>input checkbox</label> --> 
+<!-- <label>input checkbox</label> --> 
+<label>input radio</label>    <input type="radio" value="1" /> 1 <input type="radio" value="2" /> 2 <input type="radio" value="3" /> 3 <br><br>
+<label>input file</label>     <input type="file" ><br><br>
+<label>input hidden</label>     <input type="hidden" ><br><br>
+<label>input submit</label>     <input type="submit" ><br><br>
+<label>input reset</label>      <input type="reset" name="reset" value="reset" /> <br> <br>
+<label>input button</label>      <input type="button" value="button" /> <br> <br>
+<label>input gambar</label>      <input type="image" src="https://tiniru.com/assets/img/favicon.png"  name="image" />
+</form>
